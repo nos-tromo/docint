03387fa4da5bea02a39d7ef4a17ee5d4d8746239
nos-tromo <91366661+nos-tromo@users.noreply.github.com>
2025-08-24 02:14:26 +0200

    fix(frontend): force dark theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import system from "./theme";
+
+// Force dark mode regardless of prior preference
+localStorage.setItem("chakra-ui-color-mode", "dark");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider value={system}>
+      <ColorModeScript initialColorMode="dark" />
       <App />
     </ChakraProvider>
   </React.StrictMode>
