2927e4790d1ccf71295c12d48b5816aabe7292c8
nos-tromo <91366661+nos-tromo@users.noreply.github.com>
2025-12-09 14:33:07 +0100

    refactor: reorganize environment configuration and enhance PathConfig structure

--- backend/docint/utils/env_cfg.py
+++ backend/docint/utils/env_cfg.py
@@ -1,11 +1,6 @@
 import os
-import sys
 from dataclasses import dataclass
 from pathlib import Path
-
-# Ensure backend is in path if running as script
-if __name__ == "__main__":
-    sys.path.append(str(Path(__file__).parents[2].resolve()))
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -17,18 +12,94 @@ setup_logging()
 
 
 @dataclass(frozen=True)
-class PathConfig:
-    xdg_cache_home: Path
-    hf_hub_cache: Path
-
-
-@dataclass(frozen=True)
 class ModelConfig:
     embed_model: str
     sparse_model: str
     gen_model: str
     vision_model: str
     whisper_model: str
+
+
+@dataclass(frozen=True)
+class HostConfig:
+    ollama: str
+    qdrant: str
+
+
+@dataclass(frozen=True)
+class PathConfig:
+    data: Path
+    queries: Path
+    results: Path
+    prompts: Path
+    qdrant_collections: Path
+    required_exts: Path
+    xdg_cache_home: Path
+    hf_hub_cache: Path
+
+
+def load_host_env() -> HostConfig:
+    """
+    Loads host configuration from environment variables or defaults.
+
+    Returns:
+        HostConfig: Dataclass containing host configuration.
+    """
+    return HostConfig(
+        ollama=os.getenv("OLLAMA_HOST", "http://localhost:11434"),
+        qdrant=os.getenv("QDRANT_HOST", "http://localhost:6333"),
+    )
+
+
+def load_model_env() -> ModelConfig:
+    """
+    Loads model configuration from environment variables or defaults.
+
+    Returns:
+        ModelConfig: Dataclass containing model configuration.
+    """
+    return ModelConfig(
+        embed_model=os.getenv("EMBED_MODEL", "BAAI/bge-m3"),
+        sparse_model=os.getenv(
+            "SPARSE_MODEL", "Qdrant/all_miniLM_L6_v2_with_attentions"
+        ),
+        gen_model=os.getenv("LLM", "granite4:7b-a1b-h"),
+        vision_model=os.getenv("VLM", "qwen3-vl:8b"),
+        whisper_model=os.getenv("WHISPER_MODEL", "turbo"),
+    )
+
+
+def load_path_env() -> PathConfig:
+    """
+    Loads path configuration from environment variables or defaults.
+
+    Returns:
+        PathConfig: Dataclass containing path configuration.
+    """
+    home_dir = Path.home()
+    docint_data_dir: Path = home_dir / "docint"
+    package_dir: Path = Path(__file__).parents[1].resolve()
+    xdg_cache_home_dir: Path = home_dir / ".cache"
+    hf_hub_cache_dir: Path = xdg_cache_home_dir / "huggingface" / "hub"
+
+    return PathConfig(
+        data=Path(os.getenv("DATA_PATH", docint_data_dir / "data")).expanduser(),
+        queries=Path(
+            os.getenv("QUERIES_PATH", docint_data_dir / "queries.txt")
+        ).expanduser(),
+        results=Path(
+            os.getenv("RESULTS_PATH", docint_data_dir / "results")
+        ).expanduser(),
+        prompts=package_dir / "utils" / "prompts",
+        qdrant_collections=Path(
+            os.getenv("QDRANT_COL_DIR", "qdrant_collections")
+        ).expanduser(),
+        required_exts=package_dir / "core" / "readers" / "required_exts.txt",
+        xdg_cache_home=Path(
+            os.getenv("XDG_CACHE_HOME", xdg_cache_home_dir)
+        ).expanduser(),
+        hf_hub_cache=Path(os.getenv("HF_HUB_CACHE", hf_hub_cache_dir)).expanduser(),
+    )
 
 
 def set_offline_env() -> None:
@@ -60,38 +131,3 @@ def set_offline_env() -> None:
     os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
     logger.info("Set Hugging Face libraries to offline mode.")
-
-
-def load_path_env() -> PathConfig:
-    """
-    Loads path configuration from environment variables or defaults.
-
-    Returns:
-        PathConfig: Dataclass containing path configuration.
-    """
-    xdg_cache_home_dir: Path = Path.home() / ".cache"
-    hf_hub_cache_dir: Path = xdg_cache_home_dir / "huggingface" / "hub"
-    return PathConfig(
-        xdg_cache_home=Path(
-            os.getenv("XDG_CACHE_HOME", xdg_cache_home_dir)
-        ).expanduser(),
-        hf_hub_cache=Path(os.getenv("HF_HUB_CACHE", hf_hub_cache_dir)).expanduser(),
-    )
-
-
-def load_model_env() -> ModelConfig:
-    """
-    Loads model configuration from environment variables or defaults.
-
-    Returns:
-        ModelConfig: Dataclass containing model configuration.
-    """
-    return ModelConfig(
-        embed_model=os.getenv("EMBED_MODEL", "BAAI/bge-m3"),
-        sparse_model=os.getenv(
-            "SPARSE_MODEL", "Qdrant/all_miniLM_L6_v2_with_attentions"
-        ),
-        gen_model=os.getenv("LLM", "granite4:7b-a1b-h"),
-        vision_model=os.getenv("VLM", "qwen3-vl:8b"),
-        whisper_model=os.getenv("WHISPER_MODEL", "turbo"),
-    )
